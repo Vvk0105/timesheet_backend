6cc5496324d61fe892e25b6d1e93c74795f443b5
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-31 11:25:10 +0530

    deployment fixing

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenBlacklistView
+from django.http import JsonResponse
 
+def home(request):
+    return JsonResponse({"status": "ok", "message": "Timesheet API Running ✅"})
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
